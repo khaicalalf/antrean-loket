98867f433c965c922a6f634c6c5087eeadf1852f
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2024-08-06 13:32:56 +0700

    Update cetak.blade.php

--- resources/views/antrian/cetak.blade.php
+++ resources/views/antrian/cetak.blade.php
@@ -1,109 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cetak Nomor Antrian</title>
-    <style>
-        @media print {
-            @page {
-                size: 80mm 200mm;
-                /* Ukuran kertas kecil */
-                margin: 0;
-                /* Margin kertas */
-            }
+    <!-- Required meta tags -->
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='description' content=''>
+    <meta name='author' content=''>
 
-            body {
-                font-family: Arial, sans-serif;
-                color:black;
-                margin: 0;
-                padding: 0;
-                width: 80mm;
-                /* Lebar kertas */
-                height: 200mm;
-                /* Tinggi kertas */
-                background: none;
-                /* Menghilangkan background default */
-            }
 
-            .container {
-                width: 100%;
-                height: 40%;
-                padding: 10mm;
-                box-sizing: border-box;
-                position: relative;
-                text-align: center;
-                background-image: url('{{ asset('img/display-antrian.png') }}');
-                /* Gambar background */
-                background-size: cover;
-                background-position: center;
-                color: white;
-                /* Warna teks kontras */
-            }
 
-            .header {
-                margin-bottom: 15mm;
-            }
-
-            .header h2 {
-                font-size: 12px;
-                margin: 0;
-            }
-
-            .nomor-antrian {
-                font-size: 24px;
-                /* Ukuran font nomor antrian */
-                font-weight: bold;
-                margin-bottom: 15mm;
-            }
-            .poli{
-                font-size: 16px;
-                /* Ukuran font nomor antrian */
-                font-weight: bold;
-            }
-
-            .footer {
-                font-size: 10px;
-                margin-top: 10mm;
-            }
-        }
-    </style>
+    <!-- Bootstrap CSS -->
+    @vite('resources/css/app.css')
 </head>
 
-<body>
+<body style='font-family:Arial;width:auto;height:auto;margin:0;margin-left:0px;'>
+    <div class='justify-content-left'>
+        <div class='col-15'>
+            <div class='col-2'>
+                <div class='pt-2 text-center'>
+                    <span style='font-weight:bold;font-size:13px;'>Rumah Sakit Umum Daerah</span>
+                    <br />
+                    <span style='font-weight:bold;font-size:14px;'>Dr. M. H. Rabain</span>
+                </div>
 
-    <div class="container">
-        <!-- Header -->
-        <div class="header">
-            <h2>Rumah Sakit Umum Daerah</h2>
-            <h2>Dr. H. Mohamad Rabain</h2>
-        </div>
+                <div style="text-align:center;border-bottom:solid;padding-bottom:1px">
+                    <h5 style='font-size:12px;'>{{ now()->format('l, d F Y H:i') }}</h5>
+                </div>
 
-        <!-- Nomor Antrian -->
-        <div class="nomor-antrian">
-            {{ strtoupper($antrian->jenis_pasien) }} {{ $antrian->nomor_antrian }}
-            @if ($antrian->poli == 7) <br>
-            <p class="poli">Orthopedi {{ $antrian->no_antrian_poli }}</p>
-            @endif
-        </div>
+                <div style="text-align:center;border-bottom:solid;padding-bottom:1px;">
+                    <h1 style='font-size:50px;' class='fw-bold text-center lh-1'> {{ strtoupper($antrian->jenis_pasien) }} {{ $antrian->nomor_antrian }}
+                </div>
+                @if ($antrian->poli == 7) <br>
+                <p class="poli">Orthopedi {{ $antrian->no_antrian_poli }}</p>
+                @endif
 
-        <!-- Footer -->
-        <div class="footer">
-            <p>Terima kasih telah menunggu. Silakan menunggu panggilan.</p>
-            <p>{{ now()->format('l, d F Y H:i') }}</p>
+                <div class='text-center pt-1'>
+                    <span style="font-size:10px;">Jalan Sultan Mahmud Badaruddin II No. 48</span>
+                    <br />
+                    <span style="text-center;font-size:10px;">Tlp. 0734-424345</span>
+                </div>
+            </div>
         </div>
     </div>
-    <script>
-        window.onload = function() {
-            window.print(); // Menampilkan dialog cetak secara otomatis
-            window.onafterprint = function() {
-                window.location.href = "{{ url('/') }}"; // Mengarahkan pengguna kembali ke halaman index setelah cetak selesai
-            };
-        };
-    </script>
-    
-
+    </div>
+    </div>
 </body>
-
-</html>
